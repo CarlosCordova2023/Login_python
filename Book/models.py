da8607f6d46395152406933240b269f91f8b10f0
CarlosCordova2023 <learningcommun@gmail.com>
2024-09-28 19:24:04 -0300

    Add untracked files

---
+++ Book/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=150)
+    autor = models.CharField(max_length=150)
+    valoracion = models.IntegerField()
+
+    def __str__(self):
+        return self.titulo
